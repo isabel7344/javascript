f50f1322222d21dcaf340599f5369e44c86266a1
unknown <isa.fof76@gmail.com>
2020-11-10 15:08:53 +0100

    exercice4part6

--- partie6/exercice-04/assets/js/exo4.js
+++ partie6/exercice-04/assets/js/exo4.js
@@ -1,13 +1,13 @@
 let days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi','Dimanche'];
-// let gras=['Samedi','Dimanche'] texte.value.style.fontStyle = "bold";
 
-let Row = document.getElementById('Row');
+let tableau = document.getElementById('tableau');
 
 days.forEach(element => {
-    let isabelTd=document.createElement('td');
-    isabelTd.innerHTML =element;
-    Row.appendChild(isabelTd);    
+    let isabelTd = document.createElement('td');
+    isabelTd.innerHTML = element;
+    if (element == 'Samedi'|| element == 'Dimanche'){
+        isabelTd.style.fontWeight = "bold";
+    }
+    tableau.appendChild(isabelTd);    
 });
-
-
 
