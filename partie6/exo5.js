2075604ee2d5c3f5b79e9e61b23e33b4a6c43663
unknown <isa.fof76@gmail.com>
2020-11-11 17:55:27 +0100

    exo5part6

---
+++ partie6/exo5.js
@@ -0,0 +1,28 @@
+let container = document.getElementById("container");
+
+function coco() {
+  event.preventDefault();
+  let firstNumber = document.getElementById("firstNumber").value;
+  let secondNumber = document.getElementById("secondNumber").value;
+  let total = firstNumber * secondNumber;
+
+  if (
+    firstNumber > 0 &&
+    firstNumber < 11 &&
+    secondNumber > 0 &&
+    secondNumber < 11
+  ) {
+    container.innerHTML = null
+    while (total < 250) {
+      let result = document.createElement("p");
+      result.innerHTML = total;
+      container.appendChild(result);
+      secondNumber = total;
+      total = firstNumber * secondNumber;
+    }
+  } else {
+    window.alert("les nombres doivent compris entre 1 et 10 ");
+  }
+}
+
+// 
